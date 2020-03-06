786f5062849350f38d1a0ed1c3a2d3be1ccda4d2
xttong <admin@example.com>
2020-03-06 18:32:15 +0800

    123

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 引入ant的所有组件
+import './uilib/index'
+// 引入ant的样式  需要修改为less
+import 'ant-design-vue/dist/antd.css';
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
